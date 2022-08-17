3b40e7aed8c97478279565e6264eca6e51551ca8
sagor devnath <ssagor64@gmail.com>
2022-08-17 13:52:53 +0600

    initial state setup

--- script.js
+++ script.js
@@ -1,69 +1,12 @@
-// import Redux from 'redux';
-// select dom elements
-const counterEl = document.getElementById('counter');
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
-
-//action identifiers
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
-
-// action creators
-const increment = (value) => {
-    return {
-        type: INCREMENT,
-        payload: value,
-    }
-}
-
-const decrement = (value) => {
-    return {
-        type: DECREMENT,
-        payload: value,
-    }
-}
-
-// initial state
-const initialState = {
-    value: 0,
-};
-
-// create reducer function
-function counterReducer(state = initialState, action) {
-    if(action.type === INCREMENT) {
-        return {
-            ...state,
-            value: state.value + action.payload,
-        }
-    } else if (action.type === DECREMENT) {
-        return {
-            ...state,
-            value: state.value - action.payload,
-        }
-    } else {
-        return state;
-    }
-
-}
-
-// create store
-const store = Redux.createStore(counterReducer);
-
-const render = () => {
-    const state = store.getState();
-    counterEl.innerText = state.value.toString();
-}
-
-// update UI initially
-render();
-
-store.subscribe(render);
-
-//button click listeners
-incrementEl.addEventListener('click', () => {
-    store.dispatch(increment(5));
-})
-
-decrementEl.addEventListener('click', () => {
-    store.dispatch(decrement(2));
-})
+// Initial State
+let initialState = {
+    counters: [
+      {
+        counterID: 0,
+        value: 0,
+        incrementBy: Math.floor(Math.random() * 10) + 1,
+        decrementBy: Math.floor(Math.random() * 10) + 1,
+      },
+    ],
+    totalCounters: 1,
+  };
